3592aca24959ddd8815589039c3fa2973b0d1664
amitbm123 <amitbenm@post.bgu.ac.il>
2021-01-03 20:22:12 +0200

    read data from db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { BoardComponent } from './board/board.component';
 import { MatToolbarModule } from '@angular/material/Toolbar';
 import { MatGridListModule } from '@angular/material/Grid-List';
 import { TileComponent } from './tile/tile.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { TileComponent } from './tile/tile.component';
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatGridListModule
+    MatGridListModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
